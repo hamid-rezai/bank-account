90ce734fca2004d09c98dd4e53de5be8c4a800f8
Hamid Rezai <hrezai067@gmail.com>
2025-04-02 08:20:30 +1100

    solving problem

--- src/features/accounts/accountSlice.jsx
+++ src/features/accounts/accountSlice.jsx
@@ -43,50 +43,8 @@ const accountSlice = createSlice({
   }
 })
 
-console.log(accountSlice);
-
 export const { withdraw , requestLoan , payLoan} = accountSlice.actions;
-
 export default accountSlice.reducer;
-/*export default function accountReducer (state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return {
-        ...state,
-        balance: state.balance + action.payload, isLoading:false
-      };
-    case "account/withdraw":
-      return {
-        ...state,
-        balance: state.balance - action.payload,
-      };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      //LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payloan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    case"account/convertingCurrency":
-    return{
-      ...state , isLoading:true
-    }
-
-    default:
-      return state;
-  }
-};
-*/
-
 
 export function deposit(amount, currency) {
   if(currency === 'AUD') return { type: "account/deposit", payload: amount };
@@ -100,17 +58,3 @@ export function deposit(amount, currency) {
   dispatch({type:"account/deposit",payload:converted})
   }
 }
-
-/*
-export function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-
-export function requestLoan(amount, purpose) {
-  return { type: "account/requestLoan", payload: { amount, purpose } };
-}
-
-export function payLoan() {
-  return { type: "account/payloan" };
-}
-*/
