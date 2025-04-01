71a0bdbd5666feedff3a157ac886fdbc49599b6b
Hamid Rezai <hrezai067@gmail.com>
2025-04-02 08:42:28 +1100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Customer from './features/customers/customer'
 import AccountOperations from './features/accounts/accountOperations'
 import { useSelector } from 'react-redux'
 import {motion} from 'framer-motion'
-import BalanceDisplay from './features/accounts/BalanceDisplay'
+import BalanceDisplay from './features/accounts/BalanceDisplay.jsx'
 
 function App() {
 
